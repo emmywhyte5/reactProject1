781fc50952f9cc72d179b9c56caf982c53d04aba
emmywhyte5 <egbunaemmanuel5@gmail.com>
2025-06-10 04:11:12 +0100

    working

---
+++ header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div className='h-[100px] w-full bg-gray-800 text-white flex items-center justify-between px-4'>
+
+    </div>
+  )
+}
+
+export default header
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home.jsx'
+import Header from './header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
