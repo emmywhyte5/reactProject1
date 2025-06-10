650d96b5c8ae162d470308a5b26da990af1943c2
emmywhyte5 <egbunaemmanuel5@gmail.com>
2025-06-10 10:35:52 +0100

    yes

---
+++ src/Fate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Fate() {
+  return (
+    <div className='h-[100px] w-[100%] bg-amber-300'></div>
+  )
+}
+
+export default Fate
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home.jsx'
+import Header from './Header.jsx'
+import Fate from './Fate.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <Header/>
       <Home/>
-   
+      <Fate/>
   </StrictMode>,
 )
