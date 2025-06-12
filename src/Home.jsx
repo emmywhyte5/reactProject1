22ef8974e270269bfa0684f26f3bbc19af91a823
emmywhyte5 <egbunaemmanuel5@gmail.com>
2025-06-12 10:47:42 +0100

    sfg

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function Home() {
   return (
     <div className='h-[400px] w-[500px] bg-blue-500'>flex</div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home.jsx'
 import Header from './Header.jsx'
-import Fate from './Fate.jsx'
+import Form from './Form.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  
    <Form/>
-   
+   </>
+
   </StrictMode>,
 )
