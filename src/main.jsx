e84bd4f6fc8db555b1762a643a9482fe37706942
bigalow1 <valentinechizorom@gmail.com>
2025-06-12 11:03:52 +0100

    Reactproject1

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter,Routes,Route, Form } from 'react-router-dom'
-import Header from './Header'
-// import Home from './Home.jsx'
+// import Header from './Header'
+import Home from './Home.jsx'
 import BookSlot from './BookSlot.jsx'
-// import Header from './Header.jsx'
+import Header from './Header.jsx'
 
 
 createRoot(document.getElementById('root')).render(
@@ -13,7 +13,9 @@ createRoot(document.getElementById('root')).render(
  <BrowserRouter>
  <Header/>
  <Routes>
+   <Route path="/" element={<Home/>}/>
   <Route path="/book" element={<BookSlot/>}/>
+
 
  </Routes>
  </BrowserRouter>
